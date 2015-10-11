dfe7140d8174ce81d26f0e57f40a49fee6a4ca41
Pål Ruud <ruudud@gmail.com>
2015-10-11 17:07:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Send temperature to Graphite
+
+Uses connected USB TEMPer device to do room temperature reading, and optionally
+sends data to a graphite instance.
+
+Tested using [this device][], but others may work as long as they identify them
+selves using a USB ID matching **0c45:7401 Microdia** (check using `lsusb`).
+
+## Using
+```
+$ ./temp.py -h
+usage: temp.py [-h] [--no-send] [--host host] [--port port] [--metric metric]
+
+Send temperature to graphite using TEMPer device.
+
+optional arguments:
+    -h, --help       show this help message and exit
+    --no-send        do not send reading to graphite server (default: Do send)
+    --host host      address of graphite server (default: localhost)
+    --port port      port of graphite server (default: 2003)
+    --metric metric  graphite metric name (default: local.temp)
+```
+
+Do reading and send to remote graphite server (**NOTE** `sudo`):
+```
+$ sudo ./temp.py --host graphite.local --metric kitchen.temp
+Got temperature reading of 27.6°C
+Sending to Graphite on graphite.local:2003...
+```
+
+## Requirements
+**pyusb** (libusb bindings for Python) is required for reading temperatures.
+Use `pip install`, or install equivalent system package, eg. `apt-get install
+python-usb` on Debian/Ubuntu.
+
+
+## Caveats
+Escalated privileges are often required for communicating with the USB device.
+To easen up this requirement, create `/etc/udev/rules.d/99-tempsensor.rules`
+with the following content:
+
+```
+SUBSYSTEMS=="usb", ACTION=="add", ATTRS{idVendor}=="0c45", ATTRS{idProduct}=="7401", MODE="666"
+```
+
+  
+
+
+## Credits
+Magic values stolen from https://github.com/padelt/temper-python
+
+
+## License
+MIT
+
+
+[this device]: http://www.dx.com/p/temper-usb-thermometer-temperature-recorder-for-pc-laptop-81105
---
+++ temp.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# encoding: utf-8
+import argparse
+import socket
+import struct
+import time
+import usb
+
+# Some magic values to be able to read from TEMPer devices presenting
+# a USB ID like this: "0c45:7401 Microdia" (check `lsusb`)
+VID = 0x0c45L
+PID = 0x7401L
+TIMEOUT = 4000
+COMMANDS = {
+    'temp': '\x01\x80\x33\x01\x00\x00\x00\x00',
+    'ini1': '\x01\x82\x77\x01\x00\x00\x00\x00',
+    'ini2': '\x01\x86\xff\x01\x00\x00\x00\x00',
+}
+REQ_INT_LEN = 8
+ENDPOINT = 0x82
+INTERFACE = 1
+
+def _send_data(addr, message):
+  sock = socket.socket()
+  timeout_in_s = 2
+  sock.settimeout(timeout_in_s)
+  try:
+    sock.connect(addr)
+  except socket.timeout:
+    print "Took over %d second(s) to connect to %s" % (timeout_in_s, addr)
+    return
+  except Exception as error:
+    print "Unknown exception while connection to %s: %s" % (addr, error)
+    return
+
+  try:
+    sock.sendall(message)
+  except Exception as error:
+    print "Unknown exception while sending to %s: %s" % (addr, error)
+    return
+
+  print "Data sent successfully."
+  sock.shutdown(1)
+
+
+def _ctrl(dev, data):
+  dev.ctrl_transfer(bmRequestType=0x21, bRequest=0x09, wValue=0x0200,
+          wIndex=0x01, data_or_wLength=data, timeout=TIMEOUT)
+
+def _read(dev):
+  return dev.read(ENDPOINT, REQ_INT_LEN, interface=INTERFACE, timeout=TIMEOUT)
+
+def _get_device():
+  return usb.core.find(find_all=True, idVendor=VID, idProduct=PID)[0]
+
+def _take_control(dev):
+  try:
+    dev.detach_kernel_driver(INTERFACE)
+  except usb.USBError:
+    pass
+  dev.set_configuration()
+
+def _temp_reading(dev):
+  if dev.is_kernel_driver_active(INTERFACE):
+    _take_control(dev)
+
+  usb.util.claim_interface(dev, INTERFACE)
+
+  _ctrl(dev, COMMANDS['temp'])
+  _read(dev)
+  _ctrl(dev, COMMANDS['ini1'])
+  _read(dev)
+  _ctrl(dev, COMMANDS['ini2'])
+  _read(dev)
+  _read(dev)
+  _ctrl(dev, COMMANDS['temp'])
+  data = _read(dev)
+
+  usb.util.release_interface(dev, INTERFACE)
+  dev.reset()
+  return data
+
+def _to_c(data):
+  data_s = "".join([chr(byte) for byte in data])
+  temp_c = 125.0/32000.0*(struct.unpack('>h', data_s[2:4])[0])
+  return temp_c
+  return "%0.1f" % temp_c
+
+def get_reading():
+  return _to_c(_temp_reading(_get_device()))
+
+def send_to_graphite(addr, metric, value):
+  timestamp = int(time.time())
+  message = "%s %f %d\n" % (metric, value, timestamp)
+  _send_data(addr, message)
+
+def cli():
+  parser = argparse.ArgumentParser(
+    description='Send temperature to graphite using TEMPer device.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('--no-send', dest='send', default='Do send',
+          action='store_false',
+          help='do not send reading to graphite server')
+  parser.add_argument('--host', metavar='host', type=str, default='localhost',
+          help='address of graphite server')
+  parser.add_argument('--port', metavar='port', type=int, default=2003,
+          help='port of graphite server')
+  parser.add_argument('--metric', metavar='metric', type=str,
+          default='local.temp', help='graphite metric name')
+  args = parser.parse_args()
+  addr = (args.host, args.port)
+  metric = args.metric
+  do_send = args.send
+
+  temp = get_reading()
+  print "Temperature reading: %0.1f°C" % temp
+
+  if do_send:
+    print "Sending to Graphite on %s:%d..." % addr
+    send_to_graphite(addr, metric, temp)
+
+if __name__ == '__main__':
+  cli()
+
